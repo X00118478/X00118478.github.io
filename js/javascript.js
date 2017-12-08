999c152331743ae714d395bbd335e60eefe260bf
Alex Mac Uaid <x00118478@gmail.com>
2017-12-08 20:18:56 +0000

    Left Align Home Page, Removed three text boxes on Home page, added pop up on Submition click

---
+++ js/javascript.js
@@ -0,0 +1,11 @@
+
+	function confirmation() {
+			var answer = confirm("Contacting Alex Mac Uaid")
+			if (answer){
+					alert("Bye bye!")
+				//	window.location = "";
+			}
+			else{
+					alert("Thanks for sticking around!")
+			}
+	}
